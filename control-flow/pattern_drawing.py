d7f4841344d5f67ab82595b92ffd861961d7c6d4
EmmanuelAsare <asare99emmanuel10@gmail.com>
2024-10-25 14:26:26 +0000

    modified file

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,5 +1,7 @@
 p = int(input("Enter the size of the pattern: "))
-for i in range(p):
-    for j in range(p):
-        print("*",end=" ")
-    print()
+j = 1
+while j <= p:
+    print("*"* p)
+    j += 1
+print()
+
