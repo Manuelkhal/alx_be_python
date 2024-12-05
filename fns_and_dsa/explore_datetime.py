71aab5e94c484aef6d2fcec6ad8b567eeceee79e
EmmanuelAsare <asare99emmanuel10@gmail.com>
2024-12-05 11:21:31 +0000

    Modified file

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,22 +1,15 @@
-import datetime
+from datetime import datetime, timedelta
 
 def display_current_datetime():
-    current_date = datetime.datetime.now()
+    current_date = datetime.now()
     formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
     print("Current date and time:", formatted_date)
+display_current_datetime()
 
-def calculate_future_date(number_of_days):
-    current_date = datetime.datetime.now()
-    future_date = current_date + datetime.timedelta(days=number_of_days)
-    formatted_future_date = future_date .strftime("%Y-%m-%d")
-    print(f"Future Date:", formatted_future_date)
-
-
-if __name__ == "__main__":
-    display_current_datetime()
-
-    try:
-        number_of_days = int(input("Enter the number of days to add to the current date: "))
-        calculate_future_date(number_of_days)
-    except ValueError:
-        print("Please enter a valid integer for the number of days.")
+def calculate_future_date():
+    current_date = datetime.now()
+    number_of_days = int(input("Enter the number of days to add to the current date: "))
+    future_date = current_date + timedelta(days=number_of_days)
+    formatted_future_date = future_date.strftime("%Y-%m-%d")
+    print("Future date:", formatted_future_date)
+calculate_future_date()
